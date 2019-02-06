e85a6a6ed3b472aff24e5efa3ce55cf5a4b4e4bf
Lazarev Andrey <lazarevand1997@gmail.com>
2019-02-07 00:47:41 +0300

    fix /lk bug

--- client/src/Pages/PersonalPage.js
+++ client/src/Pages/PersonalPage.js
@@ -1,16 +1,56 @@
 import React, { Component } from 'react';
 import PersonalPageNav from "../Components/PersonalPageNav/PersonalPageNav";
 import WaterUserTable from "../Components/WaterUserTable/WaterUserTable";
+import axios from 'axios';
+import LoginModal from '../Components/LoginModal/LoginModal';
 
 class PersonalPage extends Component {
 
-  render() {
-    return (
-      <div>
-        <PersonalPageNav/>
-        <WaterUserTable/>
-      </div>
+    constructor(props) {
+      super(props);
+      this.state = {
+        username: null,
+        isadmin:false
+      };
+    }
+
+    componentDidMount() {
+    axios.defaults.headers.common.authorization = localStorage.getItem(
+      "access_token"
     );
+    axios
+      .get("/api/check")
+      .then(res => {
+        if (res.data.username) {
+          this.setState({
+            username: res.data.username,
+            isadmin: res.data.isadmin
+          });
+        }
+      })
+      .catch(err => console.log(err));
+  }
+
+  render() {
+      let username = this.state.username;
+      let isadmin = this.state.isadmin;
+      if(username && !isadmin){
+            return (
+              <div>
+                <PersonalPageNav/>
+                <WaterUserTable/>
+              </div>
+            );
+        } else if((username === "admin") && isadmin){
+            window.location.href = "/admin";
+        } else {
+            return (
+              <div className="container col-10 col-sm-10 col-md-6 col-lg-4 mt-4">
+                <h5 className="mb-3">You need to sign in</h5>
+                <LoginModal/>
+              </div>
+            );
+        }
   }
 }
 

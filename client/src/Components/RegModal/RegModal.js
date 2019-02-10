3ad974fe6295b2b9f8551a6482fd45bafe90b72d
Lazarev Andrey <lazarevand1997@gmail.com>
2019-02-10 14:47:02 +0300

    Register validation input fields

--- client/src/Components/RegModal/RegModal.js
+++ client/src/Components/RegModal/RegModal.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Form, FormGroup, Label, Input } from 'reactstrap';
+import { Form, FormGroup, Label, Input, FormFeedback } from 'reactstrap';
 import axios from 'axios';
 
 class RegModal extends Component {
@@ -35,23 +35,31 @@ class RegModal extends Component {
     };
 
     signUp() {
-        axios
-          .post("/api/signup", {
-            login: this.state.reg_login,
-            firstname: this.state.reg_firstname,
-            lastname: this.state.reg_lastname,
-            address: this.state.reg_address
-          })
-          .then(res => {
-            if(res.data.command === "INSERT"){
-                this.setState({ sended: true});
-            } else {
-                this.setState({ sended: false });
-            }
-          })
-          .catch(function(error) {
-            console.log(error);
-          });
+        if((this.state.reg_login !== '') && (this.state.reg_firstname !== '')
+        && (this.state.reg_lastname !== '') && (this.state.reg_address !== '')){
+            axios
+              .post("/api/signup", {
+                login: this.state.reg_login,
+                firstname: this.state.reg_firstname,
+                lastname: this.state.reg_lastname,
+                address: this.state.reg_address
+              })
+              .then(res => {
+                if(res.data.command === "INSERT"){
+                    this.setState({ sended: true});
+                } else {
+                    this.setState({ sended: false });
+                }
+              })
+              .catch(function(error) {
+                console.log(error);
+              });
+          } else {
+                var forms = document.getElementsByClassName('reg_form_valid');
+                Array.prototype.filter.call(forms, function(form) {
+                    form.classList.add('was-validated');
+                });
+          }
     };
 
 
@@ -66,22 +74,26 @@ class RegModal extends Component {
       }
     return (
       <div>
-          <Form>
+          <Form className="reg_form_valid">
             <FormGroup>
               <Label for="regLogin">Login</Label>
-              <Input onChange={this.handleLoginChange} autoFocus type="text" name="login" id="regLogin" placeholder="login" />
+              <Input onChange={this.handleLoginChange} autoFocus type="text" name="login" id="regLogin" placeholder="login" required/>
+              <FormFeedback>Input login</FormFeedback>
             </FormGroup>
             <FormGroup>
               <Label for="regFirstName">First name</Label>
-              <Input onChange={this.handleFirstNameChange} type="text" name="regFirstName" id="regFirstName" placeholder="First name" />
+              <Input onChange={this.handleFirstNameChange} type="text" name="regFirstName" id="regFirstName" placeholder="First name" required/>
+              <FormFeedback>Input client's first name</FormFeedback>
             </FormGroup>
             <FormGroup>
               <Label for="regLastName">Last name</Label>
-              <Input onChange={this.handleLastNameChange} type="text" name="regLastName" id="regLastName" placeholder="Last name" />
+              <Input onChange={this.handleLastNameChange} type="text" name="regLastName" id="regLastName" placeholder="Last name" required/>
+              <FormFeedback>Input client's last name</FormFeedback>
             </FormGroup>
             <FormGroup>
               <Label for="regAddress">Address</Label>
-              <Input onChange={this.handleAddressChange} type="textarea" name="regAddress" id="regAddress" placeholder="address" />
+              <Input onChange={this.handleAddressChange} type="textarea" name="regAddress" id="regAddress" placeholder="address" required/>
+              <FormFeedback>Input client's address</FormFeedback>
             </FormGroup>
             <button
               onClick={this.signUp.bind(this)}
